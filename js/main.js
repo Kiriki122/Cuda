a6cfcb2844f47ea7d12c33b2c5f6845f3de0bf93
Kiriki122 <kiriki496@gmail.com>
2024-09-16 14:57:28 +0300

    add burger menu and modify js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-const selection = document.querySelectorAll(".portfolio__button");
-selection.forEach((item) => item.addEventListener("click", activeselection));
-
-function activeselection() {
-    selection.forEach((item) =>
-        item.classList.remove("portfolio__button-active"),
-    );
-    this.classList.add("portfolio__button-active");
+const burger = document.querySelector(".menu__icon");
+const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu__link");
+if (burger && menu) {
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("_active");
+        menu.classList.toggle("_active");
+    });
 }
+menuLinks.forEach((item) =>
+    item.addEventListener("click", () => {
+        burger.classList.toggle("_active");
+        menu.classList.toggle("_active");
+    }),
+);
+
+const selection = document.querySelectorAll(".portfolio__button");
+selection.forEach((item) =>
+    item.addEventListener("click", () => {
+        selection.forEach((item) =>
+            item.classList.remove("portfolio__button-active"),
+        );
+        item.classList.add("portfolio__button-active");
+    }),
+);
