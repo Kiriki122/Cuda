2c50decc521d065bec96f06e97e9ac5b67cca836
Kiriki122 <kiriki496@gmail.com>
2024-09-16 18:38:05 +0300

    final?

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 const burger = document.querySelector(".menu__icon");
 const menu = document.querySelector(".menu");
 const menuLinks = document.querySelectorAll(".menu__link");
+const body = document.body;
 if (burger && menu) {
     burger.addEventListener("click", () => {
         burger.classList.toggle("_active");
         menu.classList.toggle("_active");
+        body.classList.toggle("_lock");
     });
 }
 menuLinks.forEach((item) =>
     item.addEventListener("click", () => {
         burger.classList.toggle("_active");
         menu.classList.toggle("_active");
+        body.classList.toggle("_lock");
     }),
 );
 
